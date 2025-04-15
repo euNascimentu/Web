737ef7c701a5026ec8dbafc9beffe6c2179e4550
Fernando <164927005+euNascimentu@users.noreply.github.com>
2025-04-15 16:57:19 -0300

    .

--- Mobile/atividade3/App.js
+++ Mobile/atividade3/App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Image, Alert, StyleSheet, TouchableOpacity, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          color={"darkblue"}
+          title="Clique aqui"
+          onPress={() => Alert.alert('Você Apertou o botão')}>
+        </Button>
+
+        <TouchableOpacity style={styles.botao}
+            onPress={() => alert('Clicou no botao')}>
+            <Text style={styles.textBotao}>Enviar</Text>
+          </TouchableOpacity>
+      </View>
+
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 35,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightblue',
   },
-});
+  botao: {
+    marginTop: 20,
+    width: 100,
+    height: 32,
+    backgroundColor: 'darkblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    // boxShadow:'0 0 10px darkred',
+  },
+  textBotao: {
+    color: 'white',
+  }
+})
